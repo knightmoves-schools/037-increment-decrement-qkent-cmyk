533214e2321fd0e73fbd6fc9adfbe6e1f6b748e5
Kent1an <qkent@anamosa.k12.ia.us>
2026-02-09 17:56:29 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+var counter = 100
+var prefixIncrement = ++counter
+var postfixIncrement = counter++
+var postfixDecrement = counter--
+var prefixDecrement = --counter
+document.getElementById('result').innerHTML=counter
